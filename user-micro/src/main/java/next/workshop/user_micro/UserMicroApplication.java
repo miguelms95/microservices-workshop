c9e8f3d6d1c77f20f270612134462b71219d5483
Miguel Martínez Serrano <mmartinez@nextdigital.es>
2024-11-14 19:13:45 +0100

    feat: add user micro with eureka connectivity

---
+++ user-micro/src/main/java/next/workshop/user_micro/UserMicroApplication.java
@@ -0,0 +1,13 @@
+package next.workshop.user_micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMicroApplication.class, args);
+	}
+
+}
---
+++ user-micro/src/test/java/next/workshop/user_micro/UserMicroApplicationTests.java
@@ -0,0 +1,13 @@
+package next.workshop.user_micro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
