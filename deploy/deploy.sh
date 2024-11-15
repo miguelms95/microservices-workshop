9be8da30e6646dcf1bd168b90de8c40789bc1b67
Miguel Martínez Serrano <mmartinez@nextdigital.es>
2024-11-15 04:06:24 +0100

    feat: add k8s deployments and minikube commands

---
+++ deploy/deploy.sh
@@ -0,0 +1,4 @@
+kubectl apply -f eureka-deployment.yml
+kubectl apply -f gateway-deployment.yml
+kubectl apply -f order-micro-deployment.yml
+kubectl apply -f user-micro-deployment.yml
---
+++ deploy/push-docker-images.sh
@@ -0,0 +1,4 @@
+minikube image load gateway-service:latest
+minikube image load order-micro:latest
+minikube image load user-micro:latest
+minikube image load eureka-server:latest
